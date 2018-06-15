8ea26cb0dd7efb3a8189c9d2c6c2d966b6668050
Tanya Yasiuk <t.yasiuk@gmail.com>
2018-06-15 23:47:50 +0300

    pages 28 and 30(partly)

---
+++ page.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    //Задача с калькулятором
+    int a, b, result;
+    printf("Введите первое число: ");
+    scanf("%d", &a);
+
+    printf("Введите второе число: ");
+    scanf("%d", &b);
+
+    printf("Сколько будет %d умножить на %d?\n", a, b);
+    scanf("%d", &result);
+
+    if (result == a*b) {
+        printf("Good job!\n");
+    } else {
+        printf("Неа, правильный ответ - %d\n", a*b);
+    }
+    return 0;
+}
